d63b93a25c1ad034b58263271fbded97b9b298d0
Yasuaki Uechi <uetchy@randompaper.co>
2015-06-30 02:34:26 +0900

    Add sha256 hash

--- gst.rb
+++ gst.rb
@@ -3,8 +3,9 @@ require 'formula'
 class Gst < Formula
   VERSION='1.0.0'
 
-  url "https://github.com/uetchy/gst/releases/download/v#{VERSION}/darwin_amd64.zip"
   version VERSION
+  url "https://github.com/uetchy/gst/releases/download/v#{VERSION}/darwin_amd64.zip"
+  sha256 "be5bdc3c2a563e0992614857e6dcebfdac7a489a8d7122045353c477f0b5e887"
 
   def install
     bin.install 'gst'
