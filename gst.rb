570e9ae49edb0b143b5126fd758095ecf4a55232
werckerbot <pleasemailus@wercker.com>
2015-12-12 23:30:10 +0900

    Deploy from uetchy

--- gst.rb
+++ gst.rb
@@ -1,15 +1,15 @@
 require "formula"
 
 class Gst < Formula
-  version "1.2.0"
+  version "1.3.0"
 
   
   if Hardware.is_64_bit?
-    url "https://github.com/uetchy/gst/releases/download/v1.2.0/gst_darwin_amd64.zip"
-    sha256 "ed186fe5cf1a78584e4d8f30c1732805a4b1371f"
+    url "https://github.com/uetchy/gst/releases/download/v1.3.0/gst_darwin_amd64.zip"
+    sha256 "dc45beabdb384adbd3ce6a71dc06258f062bbeab"
   else
-    url "https://github.com/uetchy/gst/releases/download/v1.2.0/gst_darwin_386.zip"
-    sha256 "ee17e8f4809a77fcef129826cfd7ba45e57ac7d8"
+    url "https://github.com/uetchy/gst/releases/download/v1.3.0/gst_darwin_386.zip"
+    sha256 "24d2f6b23eb8fb274c4001c24d69bc268d3cafed"
   end
   
 
